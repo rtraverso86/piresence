10ce34b145bb7f1ee95d304b45b4a7488d0221d1
Riccardo Traverso <rtraverso86@gmail.com>
2022-02-10 00:43:00 +0100

    Hello, HA WebSockets

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use std::env;
+use tungstenite::{connect, Message};
+use url::Url;
+
 fn main() {
-    println!("Hello, world!");
+    let host = env::var("HA_HOST").expect("environment variable missing: HA_HOST");
+    let port : u16 = env::var("HA_PORT")
+        .unwrap_or("8123".into())
+        //.expect("environment variable missing: HA_PORT")
+        .parse()
+        .expect("environment variable error: HA_PORT only accepts 16 bit unsigned integers");
+    let token = env::var("HA_TOKEN")
+        .unwrap_or("ABCDEFG".into());
+
+    env_logger::init();
+    let addr = format!("{}://{}:{}/api/websocket", "ws", host, port);
+
+    let (mut socket, response) =
+        connect(Url::parse(&addr).unwrap()).expect("Can't connect");
+
+    println!("Connected to {}", &addr);
+    println!("Response HTTP code: {}", response.status());
+    println!("Response contains the following headers:");
+    for (ref header, _value) in response.headers() {
+        println!("* {}", header);
+    }
+
+    let msg = socket.read_message().expect("Error reading message");
+    println!("Received: {}", msg);
+
+    let msg = format!("{{\"type\": \"auth\", \"access_token\": \"{}\"}}", token);
+    println!("Sending: {}", msg);
+    socket.write_message(Message::Text(msg)).unwrap();
+
+    let msg = socket.read_message().expect("Error reading message");
+    println!("Received: {}", msg);
+
+    socket.close(None).expect("Error closing socket");
 }
